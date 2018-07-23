6a7797fee1082f1240e06845ffb79f18a6809f17
michaeljaan <michaelnagashekar@gmail.com>
2018-07-23 21:32:05 +0530

    added comments

--- Date/src/Date.java
+++ Date/src/Date.java
@@ -1,42 +1,42 @@
+// date class
 import java.util.Scanner;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;//import package for dateformat
+import java.util.Calendar;//import calendar
 
 public class Date
 {
-	private Calendar dob;
+	private Calendar dob;//instance member date of birth
 	private int age;
 	private int month;
 	private int day;
 
-	public Date(Calendar dob) {
+	public Date(Calendar dob)//constructor(parameteric) 
+	{
 		super();
 		this.dob = dob;
 	}
 
-	public void getAge(Calendar dob)
+	public void getAge(Calendar dob)//age calculator(member function)
 	{
 		Calendar today=Calendar.getInstance();
 		int curYear=today.get(Calendar.YEAR);
 		int dobYear=dob.get(Calendar.YEAR);
-		this.age= curYear-dobYear;
+		this.age= curYear-dobYear;//calculate years
 		
 		int curMonth=today.get(Calendar.MONTH);
 		int dobMonth=dob.get(Calendar.MONTH);
-		this.month=dobMonth-curMonth;
+		this.month=dobMonth-curMonth;//calculate month
 		
 		int curDay=today.get(Calendar.DAY_OF_MONTH);
 		int dobDay=dob.get(Calendar.DAY_OF_MONTH);
-		this.day=curDay-dobDay;
+		this.day=curDay-dobDay;//calculate days
 		if(dobMonth > curMonth)
 		{
 			this.age--;
 		}
 		
 		else if(dobMonth == curMonth)
-		{
-			
-			
+		{					
 			if(dobDay > curDay)
 			{
 				this.age--;
@@ -45,19 +45,18 @@ public class Date
 	}
 
 	
-	public int getAge() 
+	public int getAge()//getter 
 	{
 		return age;
 	}
 
-	public int getMonth() {
+	public int getMonth()//getter
+	{
 		return month;
 	}
 
-	public int getDay() {
+	public int getDay()//getter
+	{
 		return day;
-	}
-	
-	
-	
+	}	
 }
