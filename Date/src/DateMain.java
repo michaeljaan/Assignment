f98c208eb5b8d8be8d4b691d338b48d1fc0affdf
michaeljaan <michaelnagashekar@gmail.com>
2018-07-23 21:34:09 +0530

    added comments

--- Date/src/DateMain.java
+++ Date/src/DateMain.java
@@ -1,25 +1,24 @@
+//main function
 import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class DateMain {
-
+public class DateMain
+{
 	public static void main(String[] args)throws Exception 
 	{
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the date of birth in DD/MM/YYYY format:");
-		String input=scanner.nextLine();
+		String input=scanner.nextLine();//input date of birth
 		scanner.close();
 		
 		SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
 		Calendar dob=Calendar.getInstance();
 		dob.setTime(sdf.parse(input));
-		Date Age=new Date(dob);
-		Age.getAge(dob);
+		
+		Date Age=new Date(dob);//object creation
+		Age.getAge(dob);//invoking object function
 		
 		System.out.println("Age of the person is: "+Age.getAge()+" yr "+Age.getMonth()+" months "+Age.getDay()+" days old");
-		
-	
 	}
-
 }
