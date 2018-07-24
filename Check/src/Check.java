7b88c97016d6dd5a5e6f52ff40108b968eb7db1e
michaeljaan <michaelnagashekar@gmail.com>
2018-07-24 18:02:06 +0530

    Add files via upload

---
+++ Check/src/Check.java
@@ -0,0 +1,17 @@
+//Check Class
+public class Check
+{
+	private int i=10;
+	public int j=23;
+	protected int m=12;
+	int a=88;
+	
+	public void Disp()
+	{
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(m);
+		System.out.println(a);
+	}
+	
+}
