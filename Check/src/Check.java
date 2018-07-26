be8108cdb06899ac7a884746670ff7e3c9e15e9c
michaeljaan <michaelnagashekar@gmail.com>
2018-07-26 11:49:27 +0000

    Update Check.java

--- Check/src/Check.java
+++ Check/src/Check.java
@@ -1,12 +1,12 @@
 //Check Class
 public class Check
 {
-	private int i=10;
+	private int i=10;//instance member
 	public int j=23;
 	protected int m=12;
 	int a=88;
 	
-	public void Disp()
+	public void Disp()//display function
 	{
 		System.out.println(i);
 		System.out.println(j);
