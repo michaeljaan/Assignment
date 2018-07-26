9b265d1db468440ef3baa374e9b36a41e4853439
michaeljaan <michaelnagashekar@gmail.com>
2018-07-26 22:51:02 +0530

    Update Television.java

--- Television/src/Television.java
+++ Television/src/Television.java
@@ -1,46 +1,51 @@
-
+//television class
 public class Television 
 {
-	private String state;
+	private String state;//instance member
 	private int channel;
 	private int volume;
 	
 	
 	
-	public Television(String state, int channel, int volume) {
+	public Television(String state, int channel, int volume) //constructor
+	{
 		super();
 		this.state = state;
 		this.channel = channel;
 		this.volume = volume;
 	}
 
-	public int getChannel() {
+	public int getChannel() //getter
+	{
 		return channel;
 	}
 
-	public void setChannel(int channel) {
+	public void setChannel(int channel) //getter
+	{
 		this.channel = channel;
 	}
 
-	public int getVolume() {
+	public int getVolume() //getter
+	{
 		return volume;
 	}
 	
-	public void IncVolume()
+	public void IncVolume()//function to increase volume
 	{
 		this.volume+=1;
 	}
 	
-	public void DecVolume()
+	public void DecVolume()//function to decrease volume
 	{
 		this.volume-=1;
 	}
 
-	public String getState() {
+	public String getState()//getter
+	{
 		return state;
 	}
 
-	public void setState(String state)
+	public void setState(String state)//setter
 	{
 		this.state=state;
 	}
