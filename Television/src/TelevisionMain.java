d3679cf774e8ce39bb3698053725139150514aee
michaeljaan <michaelnagashekar@gmail.com>
2018-07-26 22:52:51 +0530

    Update TelevisionMain.java

--- Television/src/TelevisionMain.java
+++ Television/src/TelevisionMain.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
+//televison main function to take inputs
 public class TelevisionMain {
 
 	public static void main(String[] args)
 	{
 		Scanner scanner=new Scanner(System.in);
-		Television Remote=new Television("OFF", 1, 10);
-		int volume,channel;
+		Television Remote=new Television("OFF", 1, 10);//create object ith initial condition
+		int volume,channel;//local variables
 		
 		System.out.println("Switch ON/OFF");
-		String state=scanner.nextLine();
+		String state=scanner.nextLine();//input state
 		Remote.setState(state);
-		if(state.equals("ON"))
+		if(state.equals("ON"))//check for ON condition
 		{
 			System.out.println("Enter Channel Number:");
-			channel=scanner.nextInt();
+			channel=scanner.nextInt();//input channel number
 			Remote.setChannel(channel);
 			
 			System.out.println("Press 1 or 0 to increase or Decrease Volume:");
-			volume=scanner.nextInt();
+			volume=scanner.nextInt();//input the volume function
 			if(volume==1)
 			{
 				Remote.IncVolume();
@@ -28,7 +29,7 @@ public class TelevisionMain {
 			}
 			
 			
-			System.out.println("The Television information:");
+			System.out.println("The Television information:");//display television details
 			System.out.println("State:"+Remote.getState());
 			System.out.println("Channel:"+Remote.getChannel());
 			System.out.println("Volume:"+Remote.getVolume());
