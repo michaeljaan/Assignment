62ca404c3a24d210ab5785e1809bad729a4e9a7b
michaeljaan <michaelnagashekar@gmail.com>
2018-07-26 22:39:51 +0530

    Update Calculator.java

--- test/src/in/co/cg/test/Calculator.java
+++ test/src/in/co/cg/test/Calculator.java
@@ -1,24 +1,24 @@
 package in.co.cg.test;
-
+//calculator class to calculate arithmatic function
 public class Calculator {
 
 	public int add(int i, int j) {
-		// TODO Auto-generated method stub
+		
 		return i+j;
 	}
 
 	public int sub(int i, int j) {
-		// TODO Auto-generated method stub
+		
 		return i-j;
 	}
 
 	public int mul(int i, int j) {
-		// TODO Auto-generated method stub
+
 		return i*j;
 	}
 
 	public int div(int i, int j) {
-		// TODO Auto-generated method stub
+		
 		return i/j;
 	}
 
