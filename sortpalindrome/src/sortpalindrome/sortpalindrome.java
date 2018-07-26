5a560d4ec119f0063793016dc7e8f0986b372755
michaeljaan <michaelnagashekar@gmail.com>
2018-07-26 12:17:47 +0000

    Update sortpalindrome.java

--- sortpalindrome/src/sortpalindrome/sortpalindrome.java
+++ sortpalindrome/src/sortpalindrome/sortpalindrome.java
@@ -1,27 +1,26 @@
 package sortpalindrome;
 import java.util.Scanner;
-
+//program to sort the String aplindrome
 public class sortpalindrome
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 	Scanner scanner=new Scanner(System.in);
-	String [] str=new String[10];
-	String []rev=new String[10];
+	String [] str=new String[10];//create an  array of string
+	String []rev=new String[10];//create an array of rev string
 	int loc[]=new int[10];
 	int size[]=new int[10];
 	int id=0;
 	int temp=0;
-	
-	
-	
+		
 	for(int i=0;i<10;i++)
 	{
 		System.out.println("Enter a string");
-		str[i]=scanner.nextLine();
+		str[i]=scanner.nextLine();//Input the string
 		rev[i]="";
 		for(int j=str[i].length()-1;j>=0;j--)
 		{
-			rev[i]=rev[i] +str[i].charAt(j);
+			rev[i]=rev[i] +str[i].charAt(j);//reverse string
 		}
 	}
 	for(int i=0;i<10;i++)
@@ -36,7 +35,7 @@ public class sortpalindrome
 	{
 		size[i]=str[loc[i]].length();
 	}
-	for(int i=0;i<id;i++)
+	for(int i=0;i<id;i++)//loop for sorting
 	{
 		for(int j=0;j<id-1;j++)
 		{
@@ -54,7 +53,7 @@ public class sortpalindrome
 	System.out.println("Palindrome Strings are");
 	for(int i=0;i<id;i++)
 	{
-		System.out.println(str[loc[i]]);
+		System.out.println(str[loc[i]]);//Display the output
 	}
 }
 }
