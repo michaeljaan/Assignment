e337de750e6d21fddd0d9bc1c60b1f4b8cfae022
michaeljaan <michaelnagashekar@gmail.com>
2018-07-30 20:41:54 +0530

    Update NoOfWords.java

--- FileProgram/src/in/com/cg/files/NoOfWords.java
+++ FileProgram/src/in/com/cg/files/NoOfWords.java
@@ -1,6 +1,6 @@
 package in.com.cg.files;
 
-import java.io.BufferedReader;
+import java.io.BufferedReader;//import packages
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -9,18 +9,18 @@ public class NoOfWords {
 	public static void main(String[] args) throws IOException 
 	{
 		BufferedReader reader = null;
-		int wordcount=0;
+		int wordcount=0;//initialize the word counter
 		try {
-			reader = new BufferedReader(new FileReader("P:/Users/nagasr/Desktop/new.txt"));
+			reader = new BufferedReader(new FileReader("P:/Users/nagasr/Desktop/new.txt"));//path name
 			
-			String currentLine=reader.readLine();
+			String currentLine=reader.readLine();//read the line
 			while(currentLine != null)
 			{
-				String[] words=currentLine.split(" ");
-				wordcount=wordcount +words.length;
+				String[] words=currentLine.split(" ");//split the word
+				wordcount=wordcount +words.length;//increment the word counter
 				currentLine=reader.readLine();
 			}
-			System.out.println("number of Words: " + wordcount);
+			System.out.println("number of Words: " + wordcount);//output the number of words
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
